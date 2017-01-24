ba815dca133ee9662ea2519afc72871dd763558b
Patrick Winter <patrick.winter@uni-konstanz.de>
2017-01-24 11:34:19 +0100

    Fixed 1 bug and some code style issue in preprocessor.py.

--- smiles/preprocessor.py
+++ smiles/preprocessor.py
@@ -1,5 +1,4 @@
 import numpy
-import h5py
 from tqdm import tnrange
 from itertools import chain
 
@@ -13,7 +12,7 @@ def preprocess(in_file, out_file, smiles_column_name='structure', max_length=180
     smiles = convert_bytes_to_strings(in_file['table']['table'][smiles_column_name])
     # if max_length is none, find the biggest string in the data
     if max_length is None:
-        max_length = find_max_length(max_length)
+        max_length = find_max_length(smiles)
     # pad and cut smiles to the correct size
     pad_strings(smiles, max_length)
     # if charset is None extract the charset from the data
@@ -26,7 +25,7 @@ def preprocess(in_file, out_file, smiles_column_name='structure', max_length=180
 
 
 def find_max_length(strings):
-    max_length = 0;
+    max_length = 0
     for i in range(0, len(strings)):
         max_length = max(max_length, strings[i])
     return max_length
@@ -43,6 +42,7 @@ def spaces(n):
         string += ' '
     return string
 
+
 def extract_charset(smiles_strings):
     charset = set()
     for smiles in smiles_strings:
@@ -51,23 +51,26 @@ def extract_charset(smiles_strings):
             charset.add(character)
     return sorted(charset)
 
+
 def write_encoded_smiles(file, name, smiles, smiles_length, charset):
-    dataset = file.create_dataset(name, (len(smiles), smiles_length, len(charset)))
+    data_set = file.create_dataset(name, (len(smiles), smiles_length, len(charset)))
     for i in tnrange(len(smiles)):
         for j in range(len(smiles[i])):
-            dataset[i,j] = encode_char(smiles[i][j], charset)
+            data_set[i, j] = encode_char(smiles[i][j], charset)
+
 
 def encode_char(character, charset):
-    bitarray = numpy.ndarray(len(charset))
+    bit_array = numpy.ndarray(len(charset))
     for i in range(len(charset)):
         if character == charset[i]:
-            bitarray[i] = 1
+            bit_array[i] = 1
         else:
-            bitarray[i] = 0
-    return bitarray
+            bit_array[i] = 0
+    return bit_array
+
 
 def convert_bytes_to_strings(byte_strings):
-    strings = numpy.ndarray(shape=(len(byte_strings)), dtype=(object))
+    strings = numpy.ndarray(shape=(len(byte_strings)), dtype=object)
     for i in range(len(strings)):
         strings[i] = byte_strings[i].decode('utf-8')
     return strings
