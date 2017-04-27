91a6732a1f1a3004b0a57d03db9c0cc3cebe0e49
patrick-winter-knime <patrick.winter@uni-konstanz.de>
2017-04-27 12:06:38 +0200

    Now using save_best_only instead of early stopping.

--- smiles-vhts/util/learn.py
+++ smiles-vhts/util/learn.py
@@ -2,7 +2,8 @@ from models import cnn
 import h5py
 from os import path
 from keras import models
-from keras.callbacks import  ModelCheckpoint, ReduceLROnPlateau, Callback, EarlyStopping
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, Callback, EarlyStopping
+import numpy
 
 
 def train(train_file, validation_file, model_file, epochs, batch_size):
@@ -24,21 +25,30 @@ def train(train_file, validation_file, model_file, epochs, batch_size):
     else:
         print('Creating new model')
         model = cnn.create_model(smiles_matrix.shape[1:], classes.shape[1])
-    checkpointer = ModelCheckpoint(filepath=model_file, monitor=monitor_metric, save_best_only=False)
+    checkpointer = ModelCheckpoint(filepath=model_file, monitor=monitor_metric, save_best_only=True)
     reduce_learning_rate = ReduceLROnPlateau(monitor=monitor_metric, factor=0.2, patience=2, min_lr=0.001)
-    model_history = ModelHistory(model_file[:-3] + '-history.csv', val)
-    early_stopping = EarlyStopping(monitor=monitor_metric)
+    model_history = ModelHistory(model_file[:-3] + '-history.csv', val, monitor_metric)
     print('Training model for ' + str(epochs) + ' epochs')
     model.fit(smiles_matrix, classes, epochs=epochs, shuffle='batch', batch_size=batch_size,
-              callbacks=[early_stopping, checkpointer, reduce_learning_rate, model_history], validation_data=val_data)
+              callbacks=[checkpointer, reduce_learning_rate, model_history], validation_data=val_data)
     train_hdf5.close()
 
 
 class ModelHistory(Callback):
 
-    def __init__(self, path, with_validation):
-        self._path_ = path
+    def __init__(self, file_path, with_validation, monitor):
+        super().__init__()
+        self._file_ = None
+        self._path_ = file_path
         self._validation_ = with_validation
+        self._monitor_ = monitor
+        if 'loss' not in monitor:
+            self._best_ = float('-inf')
+            self._compare_ = numpy.greater
+        else:
+            self._best_ = float('inf')
+            self._compare_ = numpy.less
+        self._buffer_ = ''
 
     def on_train_begin(self, logs=None):
         write_header = not path.isfile(self._path_)
@@ -53,7 +63,11 @@ class ModelHistory(Callback):
         self._file_.close()
 
     def on_epoch_end(self, epoch, logs=None):
-        self._file_.write(str(logs['loss']) + ',' + str(logs['categorical_accuracy']))
+        self._buffer_ += str(logs['loss']) + ',' + str(logs['categorical_accuracy'])
         if self._validation_:
-            self._file_.write(',' + str(logs['val_loss']) + ',' + str(logs['val_categorical_accuracy']))
-        self._file_.write('\n')
+            self._buffer_ += ',' + str(logs['val_loss']) + ',' + str(logs['val_categorical_accuracy'])
+        self._buffer_ += '\n'
+        if self._compare_(logs[self._monitor_], self._best_):
+            self._best_ = logs[self._monitor_]
+            self._file_.write(self._buffer_)
+            self._buffer_ = ''
