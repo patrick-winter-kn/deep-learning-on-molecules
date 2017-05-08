30b24f23d6befbaf51728caf6c5c43d899f3c3d1
patrick-winter-knime <patrick.winter@uni-konstanz.de>
2017-05-08 16:58:18 +0200

    Added max pooling, changed convolution sizes (2 for pairs, 8 for rings, 20 for a complexer structure)

--- smiles-vhts/models/cnn.py
+++ smiles-vhts/models/cnn.py
@@ -2,17 +2,21 @@ from keras.models import Model
 from keras.layers import Input
 from keras.layers.core import Dense, Flatten, Dropout
 from keras.layers.convolutional import Convolution1D
+from keras.layers.pooling import MaxPooling1D
 
 
 def create_model(input_shape, output_size):
     input_layer = Input(shape=input_shape, name='input')
     l = Dropout(0.2, name='dropout_input')(input_layer)
-    l = Convolution1D(9, 9, activation='relu', name='convolution_1')(l)
-    l = Dropout(0.2, name='dropout_convolution_1')(l)
-    l = Convolution1D(9, 9, activation='relu', name='convolution_2')(l)
-    l = Dropout(0.2, name='dropout_convolution_2')(l)
-    l = Convolution1D(10, 11, activation='relu', name='convolution_3')(l)
-    l = Dropout(0.2, name='dropout_convolution_3')(l)
+    l = Convolution1D(2, 2, activation='relu', name='convolution_1')(l)
+    l = MaxPooling1D(2, name='pooling_1')(l)
+    l = Dropout(0.2, name='dropout_pooling_1')(l)
+    l = Convolution1D(8, 8, activation='relu', name='convolution_2')(l)
+    l = MaxPooling1D(2, name='pooling_2')(l)
+    l = Dropout(0.2, name='dropout_pooling_2')(l)
+    l = Convolution1D(20, 20, activation='relu', name='convolution_3')(l)
+    l = MaxPooling1D(2, name='pooling_3')(l)
+    l = Dropout(0.2, name='dropout_pooling_3')(l)
     l = Flatten(name='flatten_1')(l)
     l = Dense(435, activation='relu', name='dense_1')(l)
     l = Dropout(0.2, name='dropout_dense_1')(l)
