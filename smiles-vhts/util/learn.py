db90111f851a934098b78149bea65b6d98d02504
patrick-winter-knime <patrick.winter@uni-konstanz.de>
2017-06-02 16:09:44 +0200

    Improved training and model in single target training.

--- smiles-vhts/util/learn.py
+++ smiles-vhts/util/learn.py
@@ -5,6 +5,7 @@ import h5py
 from keras import models
 from keras.callbacks import Callback, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
 from models import cnn
+from progressbar import ProgressBar
 
 
 def train(train_file, validation_file, model_file, epochs, batch_size):
@@ -12,33 +13,35 @@ def train(train_file, validation_file, model_file, epochs, batch_size):
     train_hdf5 = h5py.File(train_file, 'r')
     smiles_matrix = train_hdf5['smiles_matrix']
     classes = train_hdf5['classes']
-    monitor_metric = 'acc'
     val_data = None
     if val:
         val_hdf5 = h5py.File(validation_file, 'r')
         val_smiles_matrix = val_hdf5['smiles_matrix']
         val_classes = val_hdf5['classes']
-        monitor_metric = 'enrichment_auc'
         val_data = (val_smiles_matrix, val_classes)
+    history_file = model_file[:-3] + '-history.csv'
     if path.isfile(model_file):
         print('Loading existing model ' + model_file)
         model = models.load_model(model_file)
+        epoch = sum(1 for line in open(history_file)) - 1
     else:
         print('Creating new model')
         model = cnn.create_model(smiles_matrix.shape[1:], classes.shape[1])
+        cnn.print_model(model)
+        epoch = 0
     checkpointer = ModelCheckpoint(filepath=model_file)
     #reduce_learning_rate = ReduceLROnPlateau(monitor=monitor_metric, factor=0.2, patience=2, min_lr=0.001)
     tensorboard = TensorBoard(log_dir=model_file[:-3] + '-tensorboard', histogram_freq=1, write_graph=True,
                               write_images=False, embeddings_freq=1)
-    model_history = ModelHistory(model_file[:-3] + '-history.csv', monitor_metric)
+    model_history = ModelHistory(history_file)
     print('Training model for ' + str(epochs) + ' epochs')
     # type(classes).argmax = argmax
     # class_weights = calculate_class_weights(classes)
     # model.fit(smiles_matrix, classes, epochs=epochs, shuffle='batch', batch_size=batch_size, class_weight=class_weights,
     #           callbacks=[DrugDiscoveryEval([5, 10]), checkpointer, reduce_learning_rate, tensorboard, model_history],
     #           validation_data=val_data)
-    model.fit(smiles_matrix, classes, epochs=epochs, shuffle='batch', batch_size=batch_size,
-              callbacks=[DrugDiscoveryEval([5, 10], val_data), checkpointer, tensorboard, model_history])
+    model.fit(smiles_matrix, classes, epochs=epochs, shuffle='batch', batch_size=batch_size, initial_epoch=epoch,
+              callbacks=[DrugDiscoveryEval(val_data, [5, 10], batch_size), checkpointer, tensorboard, model_history])
     train_hdf5.close()
 
 
@@ -59,18 +62,10 @@ def calculate_class_weights(classes):
 
 class ModelHistory(Callback):
 
-    def __init__(self, file_path, monitor):
+    def __init__(self, file_path):
         super().__init__()
         self._file_ = None
         self._path_ = file_path
-        self._monitor_ = monitor
-        if 'loss' not in monitor:
-            self._best_ = float('-inf')
-            self._compare_ = numpy.greater
-        else:
-            self._best_ = float('inf')
-            self._compare_ = numpy.less
-        self._buffer_ = ''
         self.log_keys = None
         self.write_header = False
 
@@ -89,30 +84,32 @@ class ModelHistory(Callback):
         line = ''
         for key in self.log_keys:
             line += str(logs[key]) + ','
-        self._buffer_ += line[:-1] + '\n'
-        if not self._monitor:
-            self._file_.write(self._buffer_)
-            self._buffer_ = ''
-        elif self._compare_(logs[self._monitor_], self._best_):
-            self._best_ = logs[self._monitor_]
-            self._file_.write(self._buffer_)
-            self._buffer_ = ''
+        self._file_.write(line[:-1] + '\n')
 
 
 class DrugDiscoveryEval(Callback):
 
-    def __init__(self, ef_percent, validation_data):
+    def __init__(self, validation_data, ef_percent, batch_size):
         super().__init__()
         self.ef_percent = ef_percent
         self.positives = None
+        self.batch_size = batch_size
         self.val_data = validation_data
+        self.positives = self.positives_count()
 
     def on_epoch_end(self, epoch, logs=None):
         if self.val_data:
             # Start with a new line, don't print right of the progressbar
             print()
-            print('Predicting with intermediate model...')
-            predictions = self.model.predict(self.val_data[0])
+            print('Predicting with intermediate model')
+            predictions = numpy.zeros(self.val_data[1].shape, self.val_data[1].dtype)
+            with ProgressBar(max_value=len(self.val_data[0])) as progress:
+                for i in range(math.ceil(self.val_data[0].shape[0] / self.batch_size)):
+                    start = i * self.batch_size
+                    end = min(self.val_data[0].shape[0], (i + 1) * self.batch_size)
+                    results = self.model.predict(self.val_data[0][start:end])
+                    predictions[start:end] = results[:]
+                    progress.update(end)
             # Get first column ([:,0], sort it (.argsort()) and reverse the order ([::-1]))
             indices = predictions[:, 0].argsort()[::-1]
             efs, eauc = self.enrichment_stats(indices)
@@ -126,12 +123,11 @@ class DrugDiscoveryEval(Callback):
             logs['diversity_ratio'] = numpy.float64(dr)
 
     def positives_count(self):
-        if self.positives is None:
-            self.positives = 0
-            for row in self.val_data[1]:
-                if numpy.where(row == max(row))[0] == 0:
-                    self.positives += 1
-        return self.positives
+        positives = 0
+        for row in self.val_data[1]:
+            if numpy.where(row == max(row))[0] == 0:
+                positives += 1
+        return positives
 
     def enrichment_stats(self, indices):
         # efs maps the percent to the number of found positives
@@ -140,23 +136,26 @@ class DrugDiscoveryEval(Callback):
             efs[percent] = 0
         found = 0
         curve_sum = 0
-        for i in range(len(indices)):
-            row = self.val_data[1][indices[i]]
-            # Check if index (numpy.where) of maximum value (max(row)) in row is 0 (==0)
-            # This means the active value is higher than the inactive value
-            if numpy.where(row == max(row))[0] == 0:
-                found += 1
-                for percent in efs.keys():
-                    # If i is still part of the fraction count the number of founds up
-                    if i < int(math.floor(len(indices)*(percent*0.01))):
-                        efs[percent] += 1
-            curve_sum += found
+        print('Calculating enrichment stats')
+        with ProgressBar(max_value=len(indices)) as progress:
+            for i in range(len(indices)):
+                row = self.val_data[1][indices[i]]
+                # Check if index (numpy.where) of maximum value (max(row)) in row is 0 (==0)
+                # This means the active value is higher than the inactive value
+                if numpy.where(row == max(row))[0] == 0:
+                    found += 1
+                    for percent in efs.keys():
+                        # If i is still part of the fraction count the number of founds up
+                        if i < int(math.floor(len(indices)*(percent*0.01))):
+                            efs[percent] += 1
+                curve_sum += found
+                progress.update(i+1)
         # AUC = sum of found positives for every x / (positives * (number of samples + 1))
         # + 1 is added to the number of samples for the start with 0 samples selected
-        auc = curve_sum / (self.positives_count() * (len(self.val_data[1]) + 1))
+        auc = curve_sum / (self.positives * (len(self.val_data[1]) + 1))
         # Turn number of found positives into enrichment factor by dividing the number of positives found at random
         for percent in efs.keys():
-            efs[percent] /= (self.positives_count() * (percent * 0.01))
+            efs[percent] /= (self.positives * (percent * 0.01))
         return efs, auc
 
     @staticmethod
