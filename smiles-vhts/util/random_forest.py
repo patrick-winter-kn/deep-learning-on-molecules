96eedf5f8f04199bc8c098b31764e9b9bd61e4e4
patrick-winter-knime <patrick.winter@uni-konstanz.de>
2017-06-13 16:55:38 +0200

    Improvements to random forest.

--- smiles-vhts/util/random_forest.py
+++ smiles-vhts/util/random_forest.py
@@ -3,8 +3,8 @@ from sklearn.externals import joblib
 import numpy
 
 
-def train(train_data_input, train_data_output, model_path, nr_trees=10000):
-    random_forest = RandomForestClassifier(n_estimators=nr_trees, min_samples_leaf=10, class_weight="balanced", n_jobs=-1, verbose=1)
+def train(train_data_input, train_data_output, model_path, nr_trees=100):
+    random_forest = RandomForestClassifier(n_estimators=nr_trees, min_samples_leaf=10, n_jobs=-1, verbose=2)
     random_forest.fit(train_data_input, train_data_output)
     joblib.dump(random_forest, model_path)
 
